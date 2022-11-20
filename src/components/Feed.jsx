7a216bc5475345085b518111016a6e67a2bf9a68
sam <samprog21@gmail.com>
2022-11-21 02:48:44 +0530

    myyupa

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,39 +1,83 @@
-import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+/** @format */
 
-import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, Sidebar } from "./";
+import React, { useEffect, useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos, Sidebar } from './';
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
-  const [videos, setVideos] = useState(null);
+	const [selectedCategory, setSelectedCategory] =
+		useState('New');
+	const [videos, setVideos] = useState(null);
 
-  useEffect(() => {
-    setVideos(null);
+	useEffect(() => {
+		setVideos(null);
 
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
-    }, [selectedCategory]);
+		fetchFromAPI(
+			`search?part=snippet&q=${selectedCategory}`,
+		).then((data) => setVideos(data.items));
+	}, [selectedCategory]);
 
-  return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
-        </Typography>
-      </Box>
+	return (
+		<Stack
+			sx={{
+				flexDirection: { sx: 'column', md: 'row' },
+			}}>
+			<Box
+				sx={{
+					height: { sx: 'auto', md: '92vh' },
+					borderRight: '1px solid #3d3d3d',
+					px: { sx: 0, md: 2 },
+				}}>
+				<Sidebar
+					selectedCategory={selectedCategory}
+					setSelectedCategory={setSelectedCategory}
+				/>
 
-      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-        </Typography>
+				<Typography
+					className='copyright'
+					variant='body2'
+					sx={{ mt: 1.5, color: '#fff' }}>
+					Copyright © 2022 JSM Media
+				</Typography>
+			</Box>
 
-        <Videos videos={videos} />
-      </Box>
-    </Stack>
-  );
+			<Box
+				p={2}
+				sx={{
+					overflowY: 'auto',
+					height: '90vh',
+					flex: 2,
+				}}>
+				<Typography
+					variant='h4'
+					fontWeight='bold'
+					mb={2}
+					sx={{ color: 'white' }}>
+					{selectedCategory}{' '}
+					<span style={{ color: '#FC1503' }}>
+						videos
+					</span>
+				</Typography>
+
+				<Videos videos={videos} />
+			</Box>
+		</Stack>
+	);
 };
 
 export default Feed;
+
+/* 
+useEffect(() => {
+	const apiFunc = async () => {
+		const data = await fetchFromAPI(
+			`search?part=snippet&q=${selectedCategory}`,
+		);
+
+		setVideos(data.items);
+	};
+	apiFunc();
+}, [selectedCategory]);
+ */
